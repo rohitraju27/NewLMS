0275c9bf748a02b9d5653c9b56324c92bc83d6fc
Aatir <aatirqureshi1@gmail.com>
2021-01-31 15:24:13 +0530

    added express error handling middleware

--- app.js
+++ app.js
@@ -36,13 +36,28 @@ const batchRouter = require('./routes/batchRoutes')
 
 app.use('/api/v1/batches',batchRouter)
 
+
 app.all('*',(req,res,next) => {
-    res.status(404).json({
-        status:'fail',
-        message:`Can't find ${req.originalUrl} on this request`
+    // res.status(404).json({
+    //     status:'fail',
+    //     message:`Can't find ${req.originalUrl} on this request`
+    // })
+    const error = new Error(`Can't find ${req.originalUrl} on this request`)
+        error.statusCode = 404
+        error.status = 'fail'
+        next(error)
+})
+
+app.use((err,req,res,next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
     })
     next()
 })
+
 
 app.listen(PORT, (err) => {
     if(err){
