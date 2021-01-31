bc865ad62883eb11da590c9b18ab4c9fecbd4406
Aatir <aatirqureshi1@gmail.com>
2021-01-31 15:12:06 +0530

    refactor api features

--- controllers/batchController.js
+++ controllers/batchController.js
@@ -1,9 +1,12 @@
 const Batch = require('../models/batch')
 
-exports.getAllBatch = async (req,res) => {
-    try {
-        // filtering
-        const queryObj = {...req.query}
+class APIFeatures{
+    constructor(query,queryString){
+        this.query = query
+        this.queryString = queryString
+    }
+    filter(){
+        const queryObj = {...this.queryString}
         const excludedFields = ['page','sort','limit','fields']
         excludedFields.forEach(el => delete queryObj[el])
         
@@ -12,36 +15,93 @@ exports.getAllBatch = async (req,res) => {
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match => `$${match}`)
 
-        let query = Batch.find(JSON.parse(queryStr))
+        this.query = this.query.find(JSON.parse(queryStr))
+        // let query = Batch.find(JSON.parse(queryStr))
+        return this
+    }
+    sort(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy) 
+        }else{
+            this.query = this.query.sort('-createdAt')
+        }
+        return this
+    }
+
+    limitFields(){
+        if(this.queryString.fields){
+            const fields = this.queryString.fields.split(',').join(' ')
+            this.query = this.query.select(fields)
+        }else{
+            this.query = this.query.select('-__v')
+        }
+        return this
+    }
+
+    pagination(){
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 50
+        const skip = (page - 1) * limit
+        this.query = this.query.skip(skip).limit(limit)
+        
+        return this
+        // if(this.queryString.page){
+        //     const newbatches = await Batch.countDocuments()
+        //     if(skip >= newbatches) throw new Error('This page does not exsist')
+        // }
+    }
+    
+}
+
+exports.getAllBatch = async (req,res) => {
+    try {
+        // filtering
+        // const queryObj = {...req.query}
+        // const excludedFields = ['page','sort','limit','fields']
+        // excludedFields.forEach(el => delete queryObj[el])
+        
+        
+        // // advanced filtering
+        // let queryStr = JSON.stringify(queryObj)
+        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match => `$${match}`)
+
+        // let query = Batch.find(JSON.parse(queryStr))
 
         // sorting
-        if(req.query.sort){
-            const sortBy = req.query.sort.split(',').join(' ')
-            query = query.sort(sortBy) 
-        }else{
-            query = query.sort('-createdAt')
-        }
+        // if(req.query.sort){
+        //     const sortBy = req.query.sort.split(',').join(' ')
+        //     query = query.sort(sortBy) 
+        // }else{
+        //     query = query.sort('-createdAt')
+        // }
 
         // limiting fields
-        if(req.query.fields){
-            const fields = req.query.fields.split(',').join(' ')
-            query = query.select(fields)
-        }else{
-            query = query.select('-__v')
-        }
+        // if(req.query.fields){
+        //     const fields = req.query.fields.split(',').join(' ')
+        //     query = query.select(fields)
+        // }else{
+        //     query = query.select('-__v')
+        // }
 
         // pagination
-        const page = req.query.page * 1 || 1
-        const limit = req.query.limit * 1 || 50
-        const skip = (page - 1) * limit
-        query = query.skip(skip).limit(limit)
+        // const page = req.query.page * 1 || 1
+        // const limit = req.query.limit * 1 || 50
+        // const skip = (page - 1) * limit
+        // query = query.skip(skip).limit(limit)
         
-        if(req.query.page){
-            const newbatches = await Batch.countDocuments()
-            if(skip >= newbatches) throw new Error('This page does not exsist')
-        }
+        // if(req.query.page){
+        //     const newbatches = await Batch.countDocuments()
+        //     if(skip >= newbatches) throw new Error('This page does not exsist')
+        // }
 
-        const batches = await query
+        const features = new APIFeatures(Batch.find(),req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .pagination()
+        
+        const batches = await features.query
         
         res.status(200).json({
             status:'success',
